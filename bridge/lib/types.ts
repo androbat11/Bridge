0ab6a20bdbd00bdba121793be5c6c5bbd2f5ba51
Androbat11 <manuelreyesjimenez11@gmail.com>
2026-07-10 17:06:26 -0400

    Added core structure

---
+++ bridge/lib/types.ts
@@ -0,0 +1,16 @@
+// https://www.markdownguide.org/basic-syntax/
+export enum MarkdownSyntaxKind {
+    Header,
+    Paragraph,
+    /* Will support code block latner */
+    OrderedList,
+    UnorderedList,
+    Bold,
+    Italic,
+    /* Link - Will also be supported later */
+}
+
+/**  
+I don't really need to 
+
+*/
